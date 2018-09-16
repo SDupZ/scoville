3e274a4b910996f315618832d976e9b98d6a56f0
Simon du Preez <s.dupreez@crimsoneducation.org>
2018-09-16 16:18:15 +1200

    Connected to postgres DB. Added plants module

--- scoville/settings.py
+++ scoville/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'plants',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'scoville.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scoville',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
